14e858249f58493958311c0f677207ffc50b3c43
rodrigosj <contato.rodrigosilv@gmail.com>
2023-02-19 19:38:42 -0300

    :art: feat: added roboto font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+       },
+    },
   },
   plugins: [],
 };
