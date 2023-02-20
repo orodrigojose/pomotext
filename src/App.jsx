472301291280757a1c63bfae8a801a9b476b64ca
rodrigosj <contato.rodrigosilv@gmail.com>
2023-02-20 16:59:16 -0300

    :sparkles: feat(main.jsx): embedded Terminal and Pomodoro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import Terminal from "./shared/components/Terminal";
+import Pomodoro from "./shared/components/Pomodoro";
+import { useEffect, useRef } from "react";
+import avaliableCommands from "./shared/commands";
 
 function App() {
+  const inputRef = useRef();
+  const PS1 = "$ ";
+
+  useEffect(() => inputRef.current.focus(), []);
+
   return (
-    <div>
-      <p className="underline">App</p>
+    <div className="flex w-screen" onClick={() => inputRef.current.focus()}>
+      <Terminal
+        inputRef={inputRef}
+        avaliableCommands={avaliableCommands}
+        PS1={PS1}
+      />
+      <Pomodoro className="w-1/2 flex-2" />
     </div>
-  )
+  );
 }
 
 export default App;
