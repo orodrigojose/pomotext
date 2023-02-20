46168d07fb3c2817ef9ae57caea7899eb7d8e102
rodrigosj <contato.rodrigosilv@gmail.com>
2023-02-20 10:17:09 -0300

    :sparkles: feat: created prompt

---
+++ src/shared/components/Prompt.jsx
@@ -0,0 +1,16 @@
+export default function Prompt({ PS1, input, setInput, inputRef, handleKeyDown }) {
+  return (
+    <div className="flex align-baseline text-sm font-Roboto font-medium">
+      <span>{PS1}</span>
+      <input
+        className="pl-1.5 text-lm bg-transparent outline-none cursor-text pointer-events-none font-Roboto font-medium"
+        ref={inputRef}
+        type="text"
+        value={input}
+        autoFocus={true}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+    </div>
+  );
+}
