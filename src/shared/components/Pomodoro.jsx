826a21227223c9622ca198fa16844cfe5db88ca6
rodrigosj <contato.rodrigosilv@gmail.com>
2023-02-22 19:23:54 -0300

    :art: style: stylized pomodoro component

--- src/shared/components/Pomodoro.jsx
+++ src/shared/components/Pomodoro.jsx
@@ -1,13 +1,14 @@
 import { useContext, useEffect, useState } from "react";
 import { PomodoroContext } from "../context/PomodoroContext";
 
-const parseTime = (time) => time < 10 ? `0${time}` : time;
+const parseTime = (time) => (time < 10 ? `0${time}` : time);
 
-export default function Pomodoro({ customMinutes = 25, customSeconds = 0 }) {
+export default function Pomodoro({ customMinutes = 2, customSeconds = 0 }) {
   const { start } = useContext(PomodoroContext);
 
   const [minutes, setMinutes] = useState(customMinutes);
   const [seconds, setSeconds] = useState(customSeconds);
+  const [breakMinutes, setBreakMinutes] = useState(1);
 
   const [breakTime, setBreakTime] = useState(false);
 
@@ -21,7 +22,7 @@ export default function Pomodoro({ customMinutes = 25, customSeconds = 0 }) {
             setSeconds(59);
             setMinutes(minutes - 1);
           } else {
-            const minutes = breakTime ? 0 : customMinutes;
+            const minutes = breakTime ? customMinutes - 1 : breakMinutes;
             const seconds = 59;
 
             setSeconds(seconds);
@@ -39,12 +40,22 @@ export default function Pomodoro({ customMinutes = 25, customSeconds = 0 }) {
   const timerSeconds = parseTime(seconds);
 
   return (
-    <section>
+    <section className="w-2/5 flex flex-col justify-center items-center bg-zinc-900 text-slate-200 border-purple-800 border-l-2">
+      {breakTime ? (
+        <div>
+          <p className="font-Ubuntu text-xs text-slate-300">
+            Break time, new section starts in:
+          </p>
+        </div>
+      ) : null}
       <div>
-        <p>{breakTime && "Break time"}</p>
-      </div>
-      <div>
-        {timerMinutes}:{timerSeconds}
+        <h1
+          className={`text-6xl text-inherit bold font-Ubuntu ${
+            !start ? "animate-pulse opacity-95 text-red-200" : null
+          }`}
+        >
+          {timerMinutes}:{timerSeconds}
+        </h1>
       </div>
     </section>
   );
