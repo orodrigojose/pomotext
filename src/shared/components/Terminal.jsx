a9a67e54f79be5b70f04f6f3f8b412ce9edf0e40
rodrigosj <contato.rodrigosilv@gmail.com>
2023-02-22 19:24:58 -0300

    :ok_hand: style(Terminal.jsx): removed overflow scrollbar

--- src/shared/components/Terminal.jsx
+++ src/shared/components/Terminal.jsx
@@ -38,7 +38,7 @@ export default function Terminal({ inputRef, avaliableCommands, PS1 }) {
   };
 
   return (
-    <div className="whitespace-pre-line text-white bg-neutral-900 w-3/5 h-screen p-2 overflow-y-scroll">
+    <div className="whitespace-pre-line text-white bg-neutral-900 w-3/5 h-screen p-2 overflow-y-scroll no-scrollbar">
       <section className="text-sm font-RobotoMono font-medium">
         {output.map((data, index) => {
           return (
